73ec15d11e691b432e830d2325d087b668730880
Leon Thompson <lthompson@mosaiclearning.com>
2022-08-13 15:09:15 -0500

    script will be ran on aws lambda

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use chrono::{Utc, DateTime, Duration};
-use discord_news_bot::{WEBHOOK_URL, RUN_FREQUENCY};
+use discord_news_bot::{WEBHOOK_URL, RUN_FREQUENCY, RSS_FEEDS};
 use reqwest::Client;
 use serde_json::{json, Value};
 use xmltojson::{to_json};
@@ -10,49 +10,50 @@ use std::{error::Error};
 async fn main() -> Result<(), Box<dyn Error>> {
     // init request client
     let client = Client::new();
-    
 
-    // GET request to rss feed
-    let get_resp = client.get("https://decrypt.co/feed")
+    for feed_idx in 0..RSS_FEEDS.len() {
+        // GET request to rss feed
+        let get_resp = client.get(RSS_FEEDS[feed_idx])
         .send()
         .await?
         .text()
         .await?;
-    
-    // convert text response to json
-    let items = text_to_json(&get_resp);
 
-    // loop over items in rss feed
-    for i in 0..items.as_array().unwrap().len() {
-        // grab current datetime
-        let time_now = Utc::now();
-        time_now.to_rfc2822();
+        // convert text response to json
+        let items = text_to_json(&get_resp);
 
-        // grab datetime of published article
-        let mut pub_date_slice: &str = &items[i]["pubDate"].to_string();
-        pub_date_slice = pub_date_slice.trim_matches('\"');
-        
-        // ensure time is in proper format
-        let result = DateTime::parse_from_rfc2822(pub_date_slice);
-        let pub_date = match result {
-            Ok(_o) => result.unwrap(),
-            Err(ref _e) => result.expect("Error parsing rfc2822 pub_date string"),
-        }; 
-        
-        // Check if article was published since the last time the script was ran
-        if time_now.signed_duration_since(pub_date) <= Duration::seconds(RUN_FREQUENCY as i64) {
-            // prep body for POST request
-            let post_body = json!({
-                "username": "CryptoBot",
-                "content": format!("{}\n{}", items[i]["title"], items[i]["link"])
-            });
+        // loop over items in rss feed
+        for idx in 0..items.as_array().unwrap().len() {
+            // grab current datetime
+            let time_now = Utc::now();
+            time_now.to_rfc2822();
 
-            // POST request to webhook
-            client.post(WEBHOOK_URL)
-                .header("Content-Type", "application/json")
-                .body(post_body.to_string())
-                .send()
-                .await?;
+            // grab datetime of published article
+            let mut pub_date_slice: &str = &items[idx]["pubDate"].to_string();
+            pub_date_slice = pub_date_slice.trim_matches('\"');
+            
+            // ensure time is in proper format
+            let result = DateTime::parse_from_rfc2822(pub_date_slice);
+            let pub_date = match result {
+                Ok(_o) => result.unwrap(),
+                Err(ref _e) => result.expect("Error parsing rfc2822 pub_date string"),
+            }; 
+            
+            // Check if article was published since the last time the script was ran
+            if time_now.signed_duration_since(pub_date) <= Duration::seconds(RUN_FREQUENCY as i64) {
+                // prep body for POST request
+                let post_body = json!({
+                    "username": "CryptoBot",
+                    "content": format!("{}\n{}", items[idx]["title"], items[idx]["link"])
+                });
+
+                // POST request to webhook
+                client.post(WEBHOOK_URL)
+                    .header("Content-Type", "application/json")
+                    .body(post_body.to_string())
+                    .send()
+                    .await?;
+            }
         }
     }
 
